c185508a5a642f2d925c42f04b16ec5ea133885c
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-11-02 20:43:25 +0300

    easy components

---
+++ components/Ptag/Ptag.tsx
@@ -0,0 +1,20 @@
+import { PtagProps } from "./Ptag.props";
+import cn from "classnames";
+import styles from "./Ptag.module.css";
+
+export const Ptag = ({
+  children,
+  fontSize = "16px",
+}: PtagProps): JSX.Element => {
+  return (
+    <p
+      className={cn(styles.Ptag, {
+        [styles.small]: fontSize == "14px",
+        [styles.regular]: fontSize == "16px",
+        [styles.large]: fontSize == "18px",
+      })}
+    >
+      {children}
+    </p>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Htag } from "../components/index";
+import { Htag, Button, Ptag } from "../components/index";
 
 export default function Home(): JSX.Element {
-  return <Htag tag="h1">Hello, world!</Htag>;
+  return (
+    <>
+      <Htag tag="h1">Hello, world!</Htag>
+      <Button appearance="primary">primary button</Button>
+      <Button appearance="ghost" arrow="right">
+        ghost button
+      </Button>
+      <Ptag fontSize="14px">123</Ptag>
+      <Ptag fontSize="16px">456</Ptag>
+      <Ptag fontSize="18px">789</Ptag>
+    </>
+  );
 }
