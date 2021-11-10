439a1fa0a34f9861b30511ef5489f10a20618db1
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-11-10 12:05:29 +0300

    layout styles with grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,23 +1,21 @@
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
 import styles from "./Footer.module.css";
+import { format } from "date-fns";
 
-export const Footer = ({ children, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
-    <div {...props}>
-      <div className={styles.footerContainer}>
-        <p className={styles.copyright}>
-          OwlTop © 2020 - 2021 Все права защищены
-        </p>
-        <div>
-          <a href="#" className={styles.link}>
-            Пользовательское соглашение
-          </a>
-          <a href="#" className={styles.link}>
-            Политика конфиденциальности
-          </a>
-        </div>
-      </div>
-    </div>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <p className={styles.copyright}>
+        OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
+      </p>
+
+      <a href="#" target="_blank" className={styles.link}>
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank" className={styles.link}>
+        Политика конфиденциальности
+      </a>
+    </footer>
   );
 };
